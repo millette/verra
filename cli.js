8e50eb2ceafb5f401c9b69a71272033aa58af765
Robin Millette <robin@millette.info>
2017-03-15 20:12:38 -0400

    Work on cli.

--- cli.js
+++ cli.js
@@ -26,10 +26,11 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 // npm
 require('dotenv-safe').load()
-
 const meow = require('meow')
 const updateNotifier = require('update-notifier')
-const verra = require('./')
+
+// self
+const Verra = require('./')
 
 updateNotifier({ pkg: require('./package.json') }).notify()
 
@@ -47,7 +48,31 @@ const cli = meow([
   '  ponies & rainbows'
 ])
 
+const verra = new Verra()
+
+verra.init()
+  .then((x) => {
+    console.log(`${x.categories.length} categories:`)
+    x.categories.forEach((y) => {
+      console.log(`${y.text} (${y.id}) at https://file.army/category/${y.path}`)
+    })
+  })
+  .catch(console.error)
+
+/*
 verra(cli.input[0] || 'unicorns')
   .then((response) => {
     console.log(response)
   })
+*/
+
+/*
+
+const verra = new Verra()
+verra.init()
+  // .then((x) => x.category('33').byUrl('https://upload.wikimedia.org/wikipedia/commons/thumb/7/74/Euthalia_aconthea-Kadavoor-2016-06-25-003.jpg/1280px-Euthalia_aconthea-Kadavoor-2016-06-25-003.jpg'))
+  // .then((x) => x.byUrl('https://pbs.twimg.com/media/C6ord3qWgAEF9pX.jpg'))
+  .then((x) => x.byFile('/home/millette/booya.jpg'))
+  .then(console.log)
+  .catch(console.error)
+*/

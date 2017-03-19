0771ebfe5b96c6875650c976768591e860d4daa3
Robin Millette <robin@millette.info>
2017-03-19 09:22:01 -0400

    Satisfy linter.

--- cli.js
+++ cli.js
@@ -44,7 +44,6 @@ const _ = require('lodash')
 const url = require('url')
 const fs = require('fs')
 const path = require('path')
-const qs = require('querystring')
 
 // self
 const Verra = require('./')
